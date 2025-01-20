7a5ac6095c9d8f8548e7a7751376b062f8271628
Randon <randon551@gmail.com>
2025-01-19 22:24:50 -0800

    Switching to raylib

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -39,7 +39,7 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("C:/Users/Randon/Desktop/Projects/game_of_life/build/build/cmake_install.cmake")
+  include("C:/Users/Randon/Desktop/Projects/game_of_life/build/_deps/raylib-build/cmake_install.cmake")
 
 endif()
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
 #include <cstdio>
+#include <chrono>
+#include <thread>
+#include "raylib.h"
 
-int main() {
-  printf("Hello");
-  return 0;
+static constexpr int window_width = 100;
+static constexpr int window_height = 100;
+static constexpr int loop_time = 10;
+
+int main()
+{
+    InitWindow(window_width, window_height, "GOL");
+
+    while (!WindowShouldClose())
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(loop_time));
+    }
+
+    CloseWindow();
+
+    return 0;
 }
